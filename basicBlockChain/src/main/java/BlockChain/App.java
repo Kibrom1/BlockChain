daebb58626e823c8585d87b04e818821162b91a0
Kibrom1 <kibmit@gmail.com>
2019-08-19 21:55:18 -0400

    Basic implementation of blockchain using java

--- basicBlockChain/src/main/java/BlockChain/App.java
+++ basicBlockChain/src/main/java/BlockChain/App.java
@@ -1,13 +1,23 @@
 package BlockChain;
 
+import java.util.ArrayList;
+
+import com.google.gson.GsonBuilder;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static ArrayList<Block> blockchain = new ArrayList<Block>(); 
+
+	public static void main(String[] args) {
+		//add our blocks to the blockchain ArrayList:
+				blockchain.add(new Block("Hi im the first block", "0"));		
+				blockchain.add(new Block("Yo im the second block",blockchain.get(blockchain.size()-1).getHash())); 
+				blockchain.add(new Block("Hey im the third block",blockchain.get(blockchain.size()-1).getHash()));
+				
+				String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);		
+				System.out.println(blockchainJson);
+	}
 }
