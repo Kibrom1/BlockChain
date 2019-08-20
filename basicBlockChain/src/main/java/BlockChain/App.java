c65ba1f6bdba2b5b94b009272c4b7255d780e74f
Kibrom1 <kibmit@gmail.com>
2019-08-19 22:09:34 -0400

    validation Added

--- basicBlockChain/src/main/java/BlockChain/App.java
+++ basicBlockChain/src/main/java/BlockChain/App.java
@@ -9,15 +9,39 @@ import com.google.gson.GsonBuilder;
  *
  */
 public class App {
-	public static ArrayList<Block> blockchain = new ArrayList<Block>(); 
+	public static ArrayList<Block> blockchain = new ArrayList<Block>();
 
 	public static void main(String[] args) {
-		//add our blocks to the blockchain ArrayList:
-				blockchain.add(new Block("Hi im the first block", "0"));		
-				blockchain.add(new Block("Yo im the second block",blockchain.get(blockchain.size()-1).getHash())); 
-				blockchain.add(new Block("Hey im the third block",blockchain.get(blockchain.size()-1).getHash()));
-				
-				String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);		
-				System.out.println(blockchainJson);
+		// add our blocks to the blockchain ArrayList:
+		blockchain.add(new Block("Hi im the first block", "0"));
+		blockchain.add(new Block("Yo im the second block", blockchain.get(blockchain.size() - 1).getHash()));
+		blockchain.add(new Block("Hey im the third block", blockchain.get(blockchain.size() - 1).getHash()));
+
+		String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+		System.out.println(blockchainJson);
+
+		isChainValid();
+	}
+
+	public static Boolean isChainValid() {
+		Block currentBlock;
+		Block previousBlock;
+
+		// loop through blockchain to check hashes:
+		for (int i = 1; i < blockchain.size(); i++) {
+			currentBlock = blockchain.get(i);
+			previousBlock = blockchain.get(i - 1);
+			// compare registered hash and calculated hash:
+			if (!currentBlock.getHash().equals(currentBlock.calculateHash())) {
+				System.out.println("Current Hashes not equal");
+				return false;
+			}
+			// compare previous hash and registered previous hash
+			if (!previousBlock.getHash().equals(currentBlock.getPreviousHash())) {
+				System.out.println("Previous Hashes not equal");
+				return false;
+			}
+		}
+		return true;
 	}
 }
